4c45c11aeaa9fa1fa19d9582608bbe27cbb8646b
qkrehwo123 <qkrehwo@gmail.com>
2025-09-05 16:05:05 +0900

    Update User.java

--- history/src/main/java/com/lms/history/users/entity/User.java
+++ history/src/main/java/com/lms/history/users/entity/User.java
@@ -12,8 +12,8 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
-    private int userId;        // 기본 키 (PK), 데이터베이스에서 `user_id` 컬럼과 매핑
-    private String userType;    // 관리자 또는 일반 유저 구분
+    private int user_id;        // 기본 키 (PK), 데이터베이스에서 `user_id` 컬럼과 매핑
+    private String user_type;    // 관리자 또는 일반 유저 구분
     private String name;        // 이름
     private String password;    // 암호화된 비밀번호
     private String email;       // 이메일
