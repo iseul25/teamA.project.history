4a2c7fa4bb36ffa3034a1c3e653a7c0b61f85356
qkrehwo123 <qkrehwo@gmail.com>
2025-09-15 15:02:47 +0900

    Update AdminService.java

--- history/src/main/java/com/lms/history/admin/service/AdminService.java
+++ history/src/main/java/com/lms/history/admin/service/AdminService.java
@@ -17,19 +17,10 @@ public class AdminService {
         this.adminRepository = adminRepository;
     }
 
-    /**
-     * 유저 정보 저장 또는 업데이트 (수정된 메서드)
-     */
     public void save(User user) {
         adminRepository.save(user);
     }
 
-    /**
-     * 이메일이 데이터베이스에 중복되는지 확인하는 메서드입니다.
-     *
-     * @param email 확인할 이메일 주소
-     * @return 중복이면 true, 아니면 false
-     */
     public boolean isEmailDuplicated(String email) {
         return adminRepository.existsByEmail(email);
     }
